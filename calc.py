c208f8553307d8286cd1ff4afea6a4fcb889f4b2
Roberto Nombela Alonso <rnombela@gamma04.aulas.gsyc.urjc.es>
2016-09-19 10:36:48 +0200

    Added multiply and division

--- calc.py
+++ calc.py
@@ -13,6 +13,17 @@ def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
 
+def divide(op1, op2):
+    """ Function to divide the operands """
+    try:
+        return op1 / op2
+    except ZeroDivisionError:
+        sys.exit("Error: Division by zero is not allowed")
+        
+def multiply(op1, op2):
+    """ Function to multiply the operands """
+    return op1 * op2        
+
 if __name__ == "__main__":
     try:
         operando1 = int(sys.argv[1])
@@ -24,6 +35,10 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+    elif sys.argv[2] == "division":
+        result = divide(operando1, operando2)
+    elif sys.argv[2] == "multiplicacion":
+        result = multiply(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
