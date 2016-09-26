b88172d899e60674caa638dde1182c40e8f081d6
robernom <rober95@hotmail.com>
2016-09-26 10:50:55 +0200

    Revisado

--- calcoo.py
+++ calcoo.py
@@ -5,13 +5,11 @@ import sys
 
 class Calculadora():
 
-	def plus(self, op1, op2):
-		return op1 + op2
+    def plus(self, op1, op2):
+        return op1 + op2
 
-	def minus(self, op1, op2):
-		return op1 - op2
-
-
+    def minus(self, op1, op2):
+        return op1 - op2
 
 if __name__ == "__main__":
     
