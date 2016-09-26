2e8f943c55fe5bbddc7df5d8abd6cacd6f51da06
robernom <rober95@hotmail.com>
2016-09-26 14:27:29 +0200

    Pasado pep8

--- calcplusplus.py
+++ calcplusplus.py
@@ -13,13 +13,13 @@ with open(sys.argv[1],'r') as fich: # Cuando acaba el with, se cierra fich
         try:
             result = int(lista[1])
             for elemento in lista[2:]:
-                result = calc4.Selector(lista[0],result,int(elemento))
+                result = calc4.Selector(lista[0], result, int(elemento))
         except ValueError:
             """ If the parameters are not integers, it may be floats """
             try:
                 result = float(lista[1])
                 for elemento in lista[2:]:
-                    result = calc4.Selector(lista[0],result,float(elemento))
+                    result = calc4.Selector(lista[0], result, float(elemento))
             except ValueError:
                 sys.exit('Error: Non numerical parameters')
         print(result)
